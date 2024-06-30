68787e192b386e52cc7e491f21c5df334475aa46
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-06-30 17:46:14 +0200

    Improve crawling by using lxml and thread pool

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,26 +1,69 @@
+# Parsing
 from bs4 import BeautifulSoup
 import requests
+# Threading
+from concurrent.futures import ThreadPoolExecutor
 
-max_depth = 2
-seeds = [
-    "https://en.wikipedia.org/wiki/Tübingen"
+# Maximum depth to crawl to
+MAX_DEPTH = 5
+# keywords = ["tübingen", "castle", "university", "museum", "food"]
+# URL seeds to start crawling from
+SEEDS = [
+    "https://www.tuebingen.de/en/",
+    #"https://en.wikipedia.org/wiki/Tübingen",
+    #"https://www.komoot.com/guide/355570/castles-in-tuebingen-district",
+    "https://www.unimuseum.uni-tuebingen.de/en/"
 ]
-do_print = True
+
+
+MAX_THREADS = 10
+
+# List of links we have found
+found_links = []
+
 
 def crawl(link, depth=0):
-    # If link is None or depth is greater than max_depth, return
-    if link is None or depth >= max_depth or not link.startswith("https"):
+    # If we have reached the maximum depth, stop
+    if depth >= MAX_DEPTH:
         return
-    
-    if do_print:
-        print("Crawling", link, "at depth", depth)
+
+    # Add the link to the list of links
+    if link not in found_links:
+        found_links.append(link)
+
+    print('\r', f"Crawling {link} at depth {depth}", end="")
 
     try:
         response = requests.get(link)
-        soup = BeautifulSoup(response.text, "html.parser")
-        return [crawl(link.get("href"), depth + 1) for link in soup.find_all("a")]
-    except:
-        return []
 
-for seed in seeds:
-    print(crawl(seed))
+        soup = BeautifulSoup(response.text, "lxml")
+
+        # Check for links
+        for link in soup.find_all("a"):
+            found_link = link.get("href")
+            
+            # If the link is not in the list of found links, crawl it
+            if found_link not in found_links:
+                if found_link.startswith("/"):
+                    domain = "/".join(response.url.split("/")[0:3])
+                    found_link = domain + found_link
+
+                crawl(found_link, depth + 1)
+    except:
+        # Do nothing if an error occurs
+        pass
+
+
+def main():
+    with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
+        futures = [executor.submit(crawl, seed) for seed in SEEDS]
+        for future in futures:
+            future.result()
+
+    print("Found", len(found_links), "links")
+    for link in found_links:
+        print(link)
+
+
+if __name__ == "__main__":
+    main()
