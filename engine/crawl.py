e1c2e16bb35344afe789ea88543947209757a8fa
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-07-01 17:02:06 +0200

    Add support to interrupt and start the crawler from a given state

--- engine/crawl.py
+++ engine/crawl.py
@@ -1,3 +1,7 @@
+##### General #####
+import json
+import os
+import sys
 ##### Parsing #####
 from bs4 import BeautifulSoup
 import requests
@@ -24,8 +28,8 @@ SEEDS = [
     "https://www.stocherkahnfahrten.com/English/Stocherkahnrennen-English.html",
     "https://www.germany.travel/en/cities-culture/tuebingen.html",
     "https://justinpluslauren.com/things-to-do-in-tubingen-germany/",
-
 ]
+
 LANGS = ["en", "en-GB", "en-US", "english"]
 
 MAX_THREADS = 10
@@ -66,96 +70,186 @@ def can_fetch(url: str) -> bool:
     return rp.can_fetch("*", url)
 
 
-def crawl(start_link: str) -> None:
+def save_state():
     """
-    Crawls a website iteratively and extracts links from HTML pages.
-
-    Parameters:
-    - `start_link` (str): The URL to start crawling from.
-
-    Example:
-    ```python
-    crawl("https://www.tuebingen.de/en/")
-    ```
+    Saves the global state to a file.
     """
-    to_crawl = [start_link]
-    visited_links = set()
 
-    while to_crawl:
-        link = to_crawl.pop()
+    global ignore_links, found_links
 
-        print(f"Crawling {link}: ...", end="")
+    # Create directory for crawler states
+    if not os.path.exists("crawler_states"):
+        os.makedirs("crawler_states")
 
-        if not link.startswith("http"):
-            print("not a valid URL")
-            continue
+    with open(f"crawler_states/global.json", "w") as f:
+        # Write it as json
+        f.write(json.dumps({
+            "ignore_links": list(ignore_links),
+            "found_links": list(found_links)
+        }))
 
-        if link in ignore_links or link in visited_links:
-            print("already ignored or visited")
-            continue
 
-        # Check if we can fetch the URL
-        if not can_fetch(link):
-            print("robots.txt disallows")
-            ignore_links.add(link)
-            continue
+def load_state():
+    """
+    Loads the global state from a file into memory.
+    """
 
-        try:
-            response = requests.get(link)
-            soup = BeautifulSoup(response.text, "lxml")
+    global ignore_links, found_links
 
-            # Check language in html-tag and in the link
-            html_lang = soup.find("html").get("lang")
-            if (html_lang is not None and html_lang not in LANGS) and not any([lang in link for lang in LANGS]):
-                print("language not supported")
+    if not os.path.exists(f"crawler_states/global.json"):
+        print("No global state found")
+        return
+
+    with open(f"crawler_states/global.json", "r") as f:
+        data = json.loads(f.read())
+        ignore_links = set(data["ignore_links"])
+        found_links = set(data["found_links"])
+
+
+class Crawler:
+    def __init__(self, identifier: str, start_link: str) -> None:
+        self.identifier = identifier
+        self.to_crawl = [start_link]
+        self.visited_links = set()
+        self.load_crawlers_state()
+
+    def save_crawlers_state(self) -> None:
+        """
+        Saves the state of the crawler to a file.
+        """
+
+        # Create directory for crawler states
+        if not os.path.exists("crawler_states"):
+            os.makedirs("crawler_states")
+
+        with open(f"crawler_states/crawler_state_{self.identifier}.json", "w") as f:
+            # Write it as json
+            f.write(json.dumps({
+                "to_crawl": self.to_crawl,
+                "visited_links": list(self.visited_links)
+            }))
+
+    def load_crawlers_state(self) -> None:
+        """
+        Loads the state of the crawler from a file into memory.
+        """
+
+        if not os.path.exists(f"crawler_states/crawler_state_{self.identifier}.txt"):
+            print(f"No state found for crawler {self.identifier}")
+            return [], []
+
+        with open(f"crawler_states/crawler_state_{self.identifier}.json", "r") as f:
+            data = json.loads(f.read())
+            self.to_crawl = data["to_crawl"]
+            self.visited_links = set(data["visited_links"])
+
+    def crawl(self) -> None:
+        """
+        Crawls a website iteratively and extracts links from HTML pages.
+
+        Example:
+        ```python
+        crawler = Crawler("0", "https://www.tuebingen.de/en/")
+        ```
+        """
+
+        while self.to_crawl:
+            # If we have reached the maximum size, stop
+            if len(found_links) >= MAX_SIZE:
+                print("max size reached")
+                break
+
+            # Get the link to crawl
+            link = self.to_crawl.pop()
+
+            print(f"Crawling {link}: ...", end="")
+
+            if not link.startswith("http"):
+                print("not a valid URL")
+                continue
+
+            if link in ignore_links or link in self.visited_links:
+                print("already ignored or visited")
+                continue
+
+            # Check if we can fetch the URL
+            if not can_fetch(link):
+                print("robots.txt disallows")
                 ignore_links.add(link)
                 continue
 
-            # Add the link to the list of links
-            if link not in found_links and link not in ignore_links:
-                found_links.add(link)
-                visited_links.add(link)
+            try:
+                response = requests.get(link)
+                soup = BeautifulSoup(response.text, "lxml")
 
-            text = soup.text.lower()
-            # Check if there is any of the required keywords in the text
-            if not any([keyword in text for keyword in REQUIRED_KEYWORDS]):
-                ignore_links.add(link)
-                print("no required keywords")
-                continue
-
-            # Check for links
-            for a_tag in soup.find_all("a", href=True):
-                found_link = a_tag.get("href")
-
-                # If the link is anchor, ignore it
-                if found_link.startswith("#"):
+                # Check language in html-tag and in the link
+                html_lang = soup.find("html").get("lang")
+                if (html_lang is not None and html_lang not in LANGS) and not any([lang in link for lang in LANGS]):
+                    print("language not supported")
+                    ignore_links.add(link)
                     continue
 
-                if found_link.startswith("/"):
-                    domain = get_domain(response.url)
-                    found_link = domain + found_link
+                text = soup.text.lower()
+                # Check if there is any of the required keywords in the text
+                if not any([keyword in text for keyword in REQUIRED_KEYWORDS]):
+                    ignore_links.add(link)
+                    print("no required keywords")
+                    continue
 
-                if found_link not in visited_links and found_link not in ignore_links:
-                    to_crawl.append(found_link)
-                    if len(found_links) >= MAX_SIZE:
-                        print("max size reached")
-                        return
-            print("done")
-        except Exception as e:
-            print("error occurred")
-            # Do nothing if an error occurs
-            continue
+                # Check for links
+                for a_tag in soup.find_all("a", href=True):
+                    found_link = a_tag.get("href")
+
+                    # If the link is anchor, ignore it
+                    if found_link.startswith("#"):
+                        continue
+
+                    if found_link.startswith("/"):
+                        domain = get_domain(response.url)
+                        found_link = domain + found_link
+
+                    if found_link not in self.visited_links and found_link not in ignore_links:
+                        self.to_crawl.append(found_link)
+
+                # Add the link to the list of links
+                if link not in found_links and link not in ignore_links:
+                    found_links.add(link)
+                    self.visited_links.add(link)
+
+                print("done")
+            except Exception as e:
+                print("error occurred")
+                # Do nothing if an error occurs
+                continue
 
 
 def main():
-    with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
-        futures = [executor.submit(crawl, seed) for seed in SEEDS]
-        for future in futures:
-            future.result()
+    crawlers = []
+    load_state()
 
-    print("Found", len(found_links), "links")
-    for link in found_links:
-        print(link)
+    try:
+        with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
+            for i, seed in enumerate(SEEDS):
+                crawler = Crawler(i, seed)
+                crawlers.append(crawler)
+                executor.submit(crawler.crawl)
+
+        for crawler in crawlers:
+            crawler.save_crawlers_state()
+
+        print("Found", len(found_links), "links")
+        for link in found_links:
+            print(link)
+
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            for crawler in crawlers:
+                crawler.save_crawlers_state()
+            save_state()
+            sys.exit(130)
+        except SystemExit:
+            os._exit(130)
 
 
 if __name__ == "__main__":
