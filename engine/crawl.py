daf8067d31eed138433c1aa24733acff34114ecb
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-07-01 16:31:52 +0200

    Change from recursive to iterative crawler

--- engine/crawl.py
+++ engine/crawl.py
@@ -7,8 +7,8 @@ import urllib.robotparser
 ##### Threading #####
 from concurrent.futures import ThreadPoolExecutor
 
-# Maximum depth to crawl to
-MAX_DEPTH = 5
+# Maximum size of the links
+MAX_SIZE = 1000
 # Keywords to search for
 # They must be present in the HTML of the page
 REQUIRED_KEYWORDS = ["tübingen", "tuebingen"]
@@ -66,80 +66,85 @@ def can_fetch(url: str) -> bool:
     return rp.can_fetch("*", url)
 
 
-def crawl(link: str, depth=0) -> None:
+def crawl(start_link: str) -> None:
     """
-    Crawls a website recursively and extracts links from HTML pages.
+    Crawls a website iteratively and extracts links from HTML pages.
 
     Parameters:
-    - `link` (str): The URL to start crawling from.
-    - `depth` (int): The current depth of the crawl.
+    - `start_link` (str): The URL to start crawling from.
 
     Example:
     ```python
     crawl("https://www.tuebingen.de/en/")
     ```
     """
-    print(f"{' ' * depth}Crawling {link} ...", end="")
-    
-    if not link.startswith("http"):
-        print("not a valid URL")
-        return
-    
-    if link in ignore_links:
-        print("already ignored")
-        return
+    to_crawl = [start_link]
+    visited_links = set()
 
-    # Check if we can fetch the URL
-    if not can_fetch(link):
-        print("robots.txt disallows")
-        ignore_links.add(link)
-        return
+    while to_crawl:
+        link = to_crawl.pop()
 
-    try:
-        response = requests.get(link)
+        print(f"Crawling {link}: ...", end="")
 
-        soup = BeautifulSoup(response.text, "lxml")
+        if not link.startswith("http"):
+            print("not a valid URL")
+            continue
 
-        # Check language in html-tag and in the link
-        html_lang = soup.find("html").get("lang")
-        if (html_lang is not None and html_lang not in LANGS) and not any([lang in link for lang in LANGS]):
-            print("language not supported")
+        if link in ignore_links or link in visited_links:
+            print("already ignored or visited")
+            continue
+
+        # Check if we can fetch the URL
+        if not can_fetch(link):
+            print("robots.txt disallows")
             ignore_links.add(link)
-            return
+            continue
 
-        # Add the link to the list of links
-        if link not in found_links and link not in ignore_links:
-            found_links.add(link)
+        try:
+            response = requests.get(link)
+            soup = BeautifulSoup(response.text, "lxml")
 
-        text = soup.text.lower()
-        # Check if there is any of the required keywords in the text
-        if not any([keyword in text for keyword in REQUIRED_KEYWORDS]):
-            ignore_links.add(link)
-            return
-
-        # Check for links
-        for link in soup.find_all("a", href=True):
-            found_link = link.get("href")
-
-            # If the link is anchor, ignore it
-            if found_link.startswith("#"):
+            # Check language in html-tag and in the link
+            html_lang = soup.find("html").get("lang")
+            if (html_lang is not None and html_lang not in LANGS) and not any([lang in link for lang in LANGS]):
+                print("language not supported")
+                ignore_links.add(link)
                 continue
 
-            # If the link is not in the list of found links, crawl it
-            if found_link not in found_links:
+            # Add the link to the list of links
+            if link not in found_links and link not in ignore_links:
+                found_links.add(link)
+                visited_links.add(link)
+
+            text = soup.text.lower()
+            # Check if there is any of the required keywords in the text
+            if not any([keyword in text for keyword in REQUIRED_KEYWORDS]):
+                ignore_links.add(link)
+                print("no required keywords")
+                continue
+
+            # Check for links
+            for a_tag in soup.find_all("a", href=True):
+                found_link = a_tag.get("href")
+
+                # If the link is anchor, ignore it
+                if found_link.startswith("#"):
+                    continue
+
                 if found_link.startswith("/"):
                     domain = get_domain(response.url)
                     found_link = domain + found_link
-                
-                if depth >= MAX_DEPTH:
-                    print(f"reached max depth of {MAX_DEPTH}")
-                    return
 
-                crawl(found_link, depth + 1)
-    except Exception as e:
-        print("error occurred: " + str(e))
-        # Do nothing if an error occurs
-        pass
+                if found_link not in visited_links and found_link not in ignore_links:
+                    to_crawl.append(found_link)
+                    if len(found_links) >= MAX_SIZE:
+                        print("max size reached")
+                        return
+            print("done")
+        except Exception as e:
+            print("error occurred")
+            # Do nothing if an error occurs
+            continue
 
 
 def main():
