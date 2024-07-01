258b80f36cbc63ba1a407cac044ae93675206b53
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-07-01 16:15:33 +0200

    Improve seeds, add black-list and improve logging

--- engine/crawl.py
+++ engine/crawl.py
@@ -15,37 +15,25 @@ REQUIRED_KEYWORDS = ["tübingen", "tuebingen"]
 # URL seeds to start crawling from
 SEEDS = [
     "https://www.tuebingen.de/en/",
-    # "https://en.wikipedia.org/wiki/Tübingen",
-    "https://www.komoot.com/guide/355570/castles-in-tuebingen-district",
-    "https://www.unimuseum.uni-tuebingen.de/en/",
-    "https://www.tuebingen-info.de",
-    "https://www.uni-tuebingen.de",
-    "https://www.tuebingen.de",
-    "https://www.tripadvisor.com/Tourism-g198539-Tubingen_Baden_Wurttemberg-Vacations.html",
-    "https://www.lonelyplanet.com/germany/baden-wurttemberg/tubingen",
-    "https://www.tuebingen-info.de/en",
-    "https://www.my-stuwe.de/en/tuebingen/",
-    "https://www.tuebingen-info.de/en/experience/shopping/farmers-market",
+    "https://www.bio.mpg.de/2923/en",
+    "https://www.uni-tuebingen.de/en/",
     "https://www.my-stuwe.de/en/",
-    "https://www.swtue.de/en/",
-    "https://en.wikipedia.org/wiki/T%C3%BCbingen",
-    "https://www.tuebingen.de/en/calendar",
-    "https://www.tuebingen-info.de/en/experience/nature",
-    "https://www.ub.uni-tuebingen.de/en/",
-    "https://www.tuebingen.de/en/7695.html",
-    "https://www.tripadvisor.com/Attractions-g198539-Activities-c20-Tubingen_Baden_Wurttemberg.html",
-    "https://www.kulturhalle-tuebingen.de",
-    "https://www.tuebingen-info.de/en/experience/nature/hiking-cycling",
-    "https://www.tagblatt.de",
-    "https://www.kunsthalle-tuebingen.de",
-    "https://www.meetup.com/topics/language-exchange/de/t%C3%BCbingen/",
-]
+    "https://www.unimuseum.uni-tuebingen.de/en/",
+    "https://www.komoot.com/guide/210692/attractions-around-tuebingen",
+    "https://hoelderlinturm.de/english/",
+    "https://www.stocherkahnfahrten.com/English/Stocherkahnrennen-English.html",
+    "https://www.germany.travel/en/cities-culture/tuebingen.html",
+    "https://justinpluslauren.com/things-to-do-in-tubingen-germany/",
 
+]
+LANGS = ["en", "en-GB", "en-US", "english"]
 
 MAX_THREADS = 10
 
 # List of links we have found
-found_links = []
+ignore_links = set()
+found_links = set()
+
 
 def get_domain(url: str) -> str:
     return "/".join(url.split("/")[0:3])
@@ -91,50 +79,65 @@ def crawl(link: str, depth=0) -> None:
     crawl("https://www.tuebingen.de/en/")
     ```
     """
-
-    # Stop if:
-    # - reached the maximum depth, stop
-    # - the link is not a valid URL
-    # - already found the link
-    if depth >= MAX_DEPTH or not link.startswith("http") or link in found_links:
+    print(f"{' ' * depth}Crawling {link} ...", end="")
+    
+    if not link.startswith("http"):
+        print("not a valid URL")
+        return
+    
+    if link in ignore_links:
+        print("already ignored")
         return
 
     # Check if we can fetch the URL
     if not can_fetch(link):
+        print("robots.txt disallows")
+        ignore_links.add(link)
         return
-
-    # Add the link to the list of links
-    if link not in found_links:
-        found_links.append(link)
 
     try:
         response = requests.get(link)
 
         soup = BeautifulSoup(response.text, "lxml")
-        
-        # Check language in html-tag
-        if not "en" in soup.html["lang"]:
+
+        # Check language in html-tag and in the link
+        html_lang = soup.find("html").get("lang")
+        if (html_lang is not None and html_lang not in LANGS) and not any([lang in link for lang in LANGS]):
+            print("language not supported")
+            ignore_links.add(link)
             return
-        
+
+        # Add the link to the list of links
+        if link not in found_links and link not in ignore_links:
+            found_links.add(link)
+
         text = soup.text.lower()
-        # Check for keywords
+        # Check if there is any of the required keywords in the text
         if not any([keyword in text for keyword in REQUIRED_KEYWORDS]):
+            ignore_links.add(link)
             return
-        
-        print(f"{' ' * depth}Crawling {link} ...")
 
         # Check for links
         for link in soup.find_all("a", href=True):
             found_link = link.get("href")
+
+            # If the link is anchor, ignore it
+            if found_link.startswith("#"):
+                continue
 
             # If the link is not in the list of found links, crawl it
             if found_link not in found_links:
                 if found_link.startswith("/"):
                     domain = get_domain(response.url)
                     found_link = domain + found_link
+                
+                if depth >= MAX_DEPTH:
+                    print(f"reached max depth of {MAX_DEPTH}")
+                    return
 
                 crawl(found_link, depth + 1)
-    except:
+    except Exception as e:
+        print("error occurred: " + str(e))
         # Do nothing if an error occurs
         pass
 
