04a798b368a8cb06bdb504fa9610a0641bbdcf8d
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-07-09 12:34:33 +0200

    Add simple flask server

---
+++ engine/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return jsonify({"Hello": "World"})
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
