4a3d0f7307387ba985bbfe5f1f74c72f38d8be45
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-07-10 21:06:33 +0200

    Finish pipeline and update requirements.txt

--- engine/main.py
+++ engine/main.py
@@ -1,12 +1,36 @@
 """
 Pipeline for Crawling, Tokenizing, and Indexing
 """
-from crawl import start_crawl
+from concurrent.futures import ThreadPoolExecutor
+
+from crawl import Crawler
 from custom_db import index_pages, access_index, save_pages
+from custom_tokenizer import Tokenizer
+import asyncio
+
+MAX_THREADS = 10
 
 if __name__ == "__main__":
-    start_crawl()  # in crawling, we also tokenize
-    # TODO: seperate crawling and tokenizing
+    try:
+        # Initialize the pipeline
+        with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
+            crawler = Crawler()
+            crawler.max_size = 1000
+            crawler.add_executor(executor)
+
+            tokenizer = Tokenizer()
+            tokenizer.add_executor(executor)
+
+            # Add the pipeline elements
+            crawler.add_next(tokenizer)
+
+            # Start the pipeline
+            asyncio.run(crawler.process())
+    except (KeyboardInterrupt, SystemExit):
+        print("Exiting...")
+        crawler.save_state()
+        print("State saved")
+
     index_pages()
     index_df = access_index()
     index_df.to_csv("inverted_index.csv")
--- engine/pipeline.py
+++ engine/pipeline.py
@@ -1,10 +1,26 @@
+from concurrent.futures import ThreadPoolExecutor, wait
+
+
 class PipelineElement:
     def __init__(self, name):
         self.name = name
-        self.next = None
+        self.next = []
+        self.executor = None
+        print(f"Initialized {self.name}")
 
-    def set_next(self, next_element):
-        self.next = next_element
+    def add_executor(self, executor):
+        self.executor = executor
 
-    def process(self, data):
+    def process(self, *args):
         raise NotImplementedError
+
+    def add_next(self, next_element):
+        self.next.append(next_element)
+
+    def call_next(self, *args):
+        futures = []
+        for element in self.next:
+            print(f"Calling next for {self.name}: {element.name}")
+            future = element.executor.submit(element.process, *args)
+            futures.append(future)
+        wait(futures)  # Wait for all futures to complete
